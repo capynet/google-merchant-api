3b24451892fe216b99d7a8e0e042ac5441328a3c
Marcelo Tosco <capy.net@gmail.com>
2025-09-25 11:44:24 +0200

    Init.

--- vite.config.ts
+++ vite.config.ts
@@ -10,13 +10,7 @@ export default defineConfig({
     }),
   ],
   server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-      },
-    },
+    port: 3000,
   },
   build: {
     outDir: 'dist',
